cda58e67f4bea662bb080794a4f702e8af24dac4
StephaneG <34553036+crocsg@users.noreply.github.com>
2023-08-18 23:28:30 +0200

    update ascii art

--- build-marlin.sh
+++ build-marlin.sh
@@ -95,7 +95,7 @@ if [[ ${success} -eq 0 ]]; then
       printf "\noutput docker dir $OUTPUT_DIR \n"
       ls -l $OUTPUT_DIR
       printf "\e[0mMD5 Checksum Validation: \e[1;32mSucceeded\n"
-      printf "\e[0mM      \n"
+      printf "\n"
       printf "####### #    # \n"
       printf "#     # #   #  \n"
       printf "#     # #  #   \n"
@@ -103,7 +103,7 @@ if [[ ${success} -eq 0 ]]; then
       printf "#     # #  #   \n"
       printf "#     # #   #  \n"
       printf "####### #    # \n"
-      printf "\e[1;32\n"                    
+      printf "\n"                    
     else
       printf "\e[0mMD5 Checksum Validation: \e[1;31mFailed\n"
       printf "\n\e[1;31mBuild failed! \e[0mCheck the output above for errors\n"
